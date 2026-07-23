cf6ddda8d3bd856e6b9a9ff6469477246a313250
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-23 08:42:42 -0900

    feat: implement authentication routes and controllers for signup, login, and logout

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,16 @@
 import express from "express"
 import dotenv from "dotenv"
 import { connectDB } from "./lib/db.js"
+import dns from "node:dns"
+dns.setServers(["1.1.1.1","1.0.0.1"])
+import authRoutes from "./routes/authRoutes.js"
 
 dotenv.config()
 
 const app=express()
 const PORT=process.env.PORT
+
+app.use("/api/auth",authRoutes)
 
 app.listen(PORT,()=>{
    console.log(`Server is running on PORT:${PORT}`)
