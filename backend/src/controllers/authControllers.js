ecdb758090a862664a9b2cca06b9805cae1b6589
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-24 08:25:05 -0900

    feat: enhance signup functionality with user validation and password hashing

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -1,6 +1,39 @@
-export const signup=(req,res)=>{
+import User from "../models/user.model.js"
+import bcrypt from "bcryptjs"
+
+export const signup=async(req,res)=>{
+   const{username,email,password}=req.body
   try {
-    const{username,email,password}=req.body
+   if(password.length<6){
+    return res.status(400).json({message:"Password must be atleast 6 characters"})
+   }
+
+   const existingEmail=await User.findOne({email})
+   const existingUsername=await User.findOne({username})
+
+   if(existingEmail){
+     return res.status(400).json({message:"Email already exists"})
+   }
+
+   if(existingUsername){
+     return res.status(400).json({message:"Username already exists"})
+   }
+
+   const salt=await bcrypt.genSalt(10)
+   const hashedPassword=await bcrypt.hash(password,salt )
+
+   const user=new User({
+    username,
+    email,
+    password:hashedPassword
+   })
+
+   if(user){
+   
+   }else{
+     return res.status(400).json({message:"Invalid user data"})
+   }
+
   } catch (error) {
     
   }
