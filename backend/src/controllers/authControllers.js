7bc732e2a15d64c47ead9b89081b81e1af1eada0
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-25 00:37:06 -0900

    feat: add updateProfile functionality to auth routes and controller

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -93,3 +93,7 @@ export const logout = (req, res) => {
     res.status(500).json({ message: "Internal Server Error" })
  }
 }
+
+export const updateProfile = (req, res) => {
+  
+}
--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -6,5 +6,6 @@ const router=express.Router()
 router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
+router.put("/updateProfile",protectRoute,updateProfile)
 
 export default router
