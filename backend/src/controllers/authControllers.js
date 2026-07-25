053a76c4237b48850e486f482c8c826ccb1cc335
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-24 23:51:11 -0900

    fix: correct error logging messages in login and logout controllers

--- backend/src/controllers/authControllers.js
+++ backend/src/controllers/authControllers.js
@@ -79,7 +79,7 @@ export const login = async (req, res) => {
         profilePic: user.profilePic
       })
   } catch (error) {
-    console.log("Error in signup controller", error.message)
+    console.log("Error in login controller", error.message)
     res.status(500).json({ message: "Internal Server Error" })
   }
 }
@@ -89,6 +89,7 @@ export const logout = (req, res) => {
   res.cookie("jwt","",{maxAge:0})
    return res.status(200).json({ message: "Logged Out Sucessfully" })
  } catch (error) {
-  
+   console.log("Error in logout controller", error.message)
+    res.status(500).json({ message: "Internal Server Error" })
  }
 }
