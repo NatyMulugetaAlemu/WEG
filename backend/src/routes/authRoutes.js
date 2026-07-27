3c7b7446f2956483a9cc7f487db23bab45ed1151
NatyMulugetaAlemu <metasebiazeleke@gmail.com>
2026-07-26 22:36:54 -0900

    feat: add checkAuth route and controller for user authentication status

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -8,5 +8,6 @@ router.post("/signup",signup)
 router.post("/login",login)
 router.post("/logout",logout)
 router.put("/updateProfile",protectRoute,updateProfile)
+router.get("/checkAuth",protectRoute,checkAuth)
 
 export default router
